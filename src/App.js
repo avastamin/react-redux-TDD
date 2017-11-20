42e4e8a35a2cbebc202d5dd1d40faa114cc56c22
Ruhul Amin <r.amin@medidate.de>
2017-11-20 18:10:56 +0100

    small work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import AddTodo from './components/addTodo';
+import PropTypes from 'prop-types';
+import actions from './actions/';
 
-const App = () =>(
-  <div>Todo List</div>
+const App = ({ submitTodo }) =>(
+  <div>
+    <h2>Todo List</h2>
+    <AddTodo submitTodo={submitTodo} />
+  </div>
 );
-export default App;
+
+App.propTypes = {
+  submitTodo: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => state.todoListApp;
+
+const mapDispatchToProps = dispatch => ({
+  submitTodo: (text) => {
+    if (text) {
+      dispatch(actions.submitTodo(text));
+    }
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,12 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 it('App renders without crashing', () => {
-  const component = shallow(<App />);
+  const mockFunction = jest.fn();
+  const component = shallow(
+    <App
+      state={initialState}
+      submitTodo={mockFunction}
+    />,
+  );
   expect(component.exists()).toEqual(true);
 });
